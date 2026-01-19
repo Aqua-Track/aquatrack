f4e2a60162164bcef9ec0a2fd59d74b68e6d533e
italoptes <jose.pontes@dcx.ufpb.br>
2026-01-19 17:03:07 -0300

    Correção bug de voltar

--- src/main/java/com/ufpb/aquatrack/tipoRacao/TipoRacaoController.java
+++ src/main/java/com/ufpb/aquatrack/tipoRacao/TipoRacaoController.java
@@ -1,16 +1,15 @@
 package com.ufpb.aquatrack.tipoRacao;
 
 import com.ufpb.aquatrack.usuario.Usuario;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.servlet.http.HttpSession;
-
 import java.math.BigDecimal;
 
 @Controller
-@RequestMapping("/racoes")
+@RequestMapping("/fazenda/{fazendaId}/racoes")
 public class TipoRacaoController {
 
     private final TipoRacaoService tipoRacaoService;
@@ -19,43 +18,53 @@ public class TipoRacaoController {
         this.tipoRacaoService = tipoRacaoService;
     }
 
+
     @GetMapping
-    public String listar(HttpSession session, Model model) {
+    public String listar(@PathVariable Long fazendaId, HttpSession session, Model model
+    ) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
 
         model.addAttribute("racoes", tipoRacaoService.listarRacoesDoUsuario(usuario));
+        model.addAttribute("idFazenda", fazendaId);
 
         return "racao/listar_tipo";
     }
 
+
     @GetMapping("/nova")
-    public String nova() {
+    public String nova(@PathVariable Long fazendaId, Model model) {
+        model.addAttribute("idFazenda", fazendaId);
         return "racao/formulario_tipo";
     }
 
 
     @PostMapping("/criar")
     public String criar(
-            @RequestParam String nome, @RequestParam String fabricante, @RequestParam Double kgPorSaco,
+            @PathVariable Long fazendaId, @RequestParam String nome,
+            @RequestParam String fabricante, @RequestParam Double kgPorSaco,
             @RequestParam BigDecimal valorPorSaco, HttpSession session, Model model
     ) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
 
         try {
             tipoRacaoService.cadastrarRacao(nome, fabricante, kgPorSaco, valorPorSaco, usuario);
-            return "redirect:/racoes";
+
+            return "redirect:/fazenda/" + fazendaId + "/racoes";
 
         } catch (IllegalArgumentException e) {
             model.addAttribute("erro", e.getMessage());
+            model.addAttribute("idFazenda", fazendaId);
             return "racao/formulario_tipo";
         }
     }
 
 
     @PostMapping("/{id}/remover")
-    public String remover(@PathVariable Long id, HttpSession session) {
+    public String remover(@PathVariable Long fazendaId, @PathVariable Long id, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuario");
+
         tipoRacaoService.removerRacao(id, usuario);
-        return "redirect:/racoes";
+
+        return "redirect:/fazenda/" + fazendaId + "/racoes";
     }
 }
