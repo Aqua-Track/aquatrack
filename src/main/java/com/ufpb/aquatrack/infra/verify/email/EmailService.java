546b7071e0a817056b6bd51d26615ba44bec1c11
Mykael Bruno <mykael.bruno@dcx.ufpb.br>
2026-01-26 16:41:14 -0300

    Novo layout do email de confirmação - TA LINDO

--- src/main/java/com/ufpb/aquatrack/infra/verify/email/EmailService.java
+++ src/main/java/com/ufpb/aquatrack/infra/verify/email/EmailService.java
@@ -1,30 +1,54 @@
 package com.ufpb.aquatrack.infra.verify.email;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import jakarta.mail.MessagingException;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
-    @Value("${aquaTrack.domain}") //Recupera o domínio do .proprieties do perfil atual
+    @Value("${aquaTrack.domain}")
     private String dominio;
 
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender, TemplateEngine templateEngine) {
         this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
     }
 
     public void enviarEmailAtivacao(String emailDestino, String token) {
+
         String linkAtivacao = dominio + "/ativar-conta?token=" + token;
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(emailDestino);
-        message.setSubject("Aquatrack - Ativação da sua conta");
-        message.setText("Clique no link abaixo para ativar sua conta do AquaTrack: \n" + linkAtivacao);
+        Context context = new Context();
+        context.setVariable("linkAtivacao", linkAtivacao);
+        context.setVariable("logoUrl", dominio + "/images/logo.png");
 
-        mailSender.send(message);
+        String html = templateEngine.process("email/ativar-conta-email", context);
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(emailDestino);
+            helper.setSubject("AquaTrack - Ativação da sua conta");
+            helper.setText(html, true);
+            // Logo embutida
+            helper.addInline("logo", new ClassPathResource("static/images/logo-simples.png"));
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException("Erro ao enviar e-mail de ativação", e);
+        }
     }
 }
